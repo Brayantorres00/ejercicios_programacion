0167fd6b550341d099ee4b9da6101dc8713228f9
Brayantorres00 <161845977+Brayantorres00@users.noreply.github.com>
2024-03-01 09:43:21 -0500

    mi primer commit

---
+++ ejecicios.js
@@ -0,0 +1,18 @@
+var nombre = "Brayan";
+var apellido = "torres";
+var edad = 17;
+var num1 = 5;
+var num2 = 3;
+
+var suma = num1 + num2;
+
+var tengoNovia = true;
+
+console.log(tengoNovia);
+console.log(suma);
+console.log(edad)
+console.log(nombre);
+console.log(apellido);
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(tengoNovia));
